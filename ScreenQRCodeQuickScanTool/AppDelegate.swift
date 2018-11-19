84703fc4a2d64fde54f770f9f774cc0bc882b8c9
yicheng <yicheng.fzu@gmail.com>
2018-11-19 10:21:30 +0800

    Initial Commit

---
+++ ScreenQRCodeQuickScanTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScreenQRCodeQuickScanTool
+//
+//  Created by CYC on 2018/11/19.
+//  Copyright © 2018 west2. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScreenQRCodeQuickScanTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenQRCodeQuickScanTool
+//
+//  Created by CYC on 2018/11/19.
+//  Copyright © 2018 west2. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
